27a5488aa03845558fe5ea1021c826e4a5f0e9e7
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-04-18 15:13:53 -0400

    Completed 1-4 string exercises

---
+++ Pythons_Lists.py
@@ -0,0 +1,35 @@
+# String Exercises 1
+"""
+string = "I don't know"
+
+print(string.upper())
+"""
+
+# String Exercise 2
+"""
+string = "sTeVe"
+
+print(string.capitalize())
+"""
+
+# String Exercise 3
+"""
+string = "dave"
+
+print(string[::-1])
+"""
+
+# String Exercise 4
+"""
+string_1 = "can you hear them? the children of the night."
+
+string_2 = string_1.replace("a","4")
+string_3 = string_2.replace("e", "3")
+string_4 = string_3.replace("g","6")
+string_5 = string_4.replace("i","1")
+string_6 = string_5.replace("o","0")
+string_7 = string_6.replace("s","5")
+string = string_7.replace("t","7")
+
+print(string.upper())
+"""
